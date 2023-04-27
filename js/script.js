e1ebc5abd3359bccdf9a4f3d02cdc39a025f59c5
Karoline Prestes <karolineprestes182@gmail.com>
2023-04-27 15:25:05 -0300

    atividade da aula 22

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const API_KEY = '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';
+const username = 'PrestesKarol';
+
+const url = `https://api.github.com/users/${username}`;
+
+fetch(url, {
+  headers: {
+    Authorization: `Bearer ${API_KEY}`
+  }
+})
+.then(response => response.json())
+.then(data => {
+  console.log(data);
+  const usernameElement = document.getElementById('username');
+  const nameElement = document.getElementById('name');
+  const bioElement = document.getElementById('bio');
+  const followersElement = document.getElementById('followers');
+  const reposElement = document.getElementById('repos');
+
+  usernameElement.innerText = data.login;
+  nameElement.innerText = `Nome: ${data.name}`;
+  bioElement.innerText = `Bio: ${data.bio}`;
+  followersElement.innerText = `Seguidores: ${data.followers}`;
+  reposElement.innerText = `Repositórios: ${data.public_repos}`;
+})
+.catch(error => {
+  console.log(error);
+});
